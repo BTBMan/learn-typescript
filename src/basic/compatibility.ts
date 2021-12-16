aaa94f47e4a6b0a9e7bddb517d0e70038f7b1b86
BTBMan <1498118157@qq.com>
2021-12-16 22:34:31 +0800

    function return type compatibility

--- src/basic/compatibility.ts
+++ src/basic/compatibility.ts
@@ -10,10 +10,10 @@ class Dog {
 }
 
 // Dog的类型兼容Person1 因为检查到Dog里有和Person1相同的name类型 所以Dog可以分配给Person1
-// 如果Dog里有其他的类型成员 则也不会报错 因为目标类型是Person1 只需要考虑Dog里是否有name就可以了 反之则会报错
+// 如果Dog里有其他的类型成员 则也不会报错 因为源类型是Person1 只需要考虑目标成员Dog里是否有name就可以了 反之则会报错
 const dog1: Person1 = new Dog();
 
-// 函数兼容
+// 函数参数兼容
 let comFn1 = (a: number) => 0;
 let comFn2 = (c: number, b: string) => 0;
 
@@ -21,3 +21,13 @@ let comFn2 = (c: number, b: string) => 0;
 comFn2 = comFn1;
 // 报错 comFn2不兼容comFn1 因为comFn1的函数参数只有一个 而comFn2的参数比comFn的多 所以comFn2不匹配comFn1
 // comFn1 = comFn2;
+
+// 函数返回值兼容
+let comFn3 = () => ({ name: 'john' });
+let comFn4 = () => ({ name: 'john', age: 18 });
+
+// 和第一种方式一下 只需要考虑返回值的类型里包不包涵comFn3的返回值类型
+// comFn4有和comFn3相同的name类型 所以ok
+comFn3 = comFn4;
+// 反之comFn3中缺少类型age 所以comFn3不兼容comFn4
+// comFn4 = comFn3
