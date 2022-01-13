5cab0e55fe0f3a14708ace03787dbb89abf4afbc
BTBMan <1498118157@qq.com>
2022-01-13 21:01:24 +0800

    returnType and omit

---
+++ src/advanced/Omit.ts
@@ -0,0 +1,81 @@
+/*
+  3 - Omit
+  -------
+  by Anthony Fu (@antfu) #medium #union #built-in
+  
+  ### Question
+  
+  Implement the built-in `Omit<T, K>` generic without using it.
+  
+  Constructs a type by picking all properties from `T` and then removing `K`
+  
+  For example
+  
+  ```ts
+  interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+  
+  type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+  
+  const todo: TodoPreview = {
+    completed: false,
+  }
+  ```
+  
+  > View on GitHub: https://tsch.js.org/3
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// 1.
+// type MyOmit<T, K extends keyof T, E extends keyof T = keyof T> = {
+//   [P in E extends K ? never : E]: T[P];
+// };
+
+// 2. is better
+// https://www.typescriptlang.org/docs/handbook/2/mapped-types.html#key-remapping-via-as
+// 把key循环出来在进行循环比较联合类型
+type MyOmit<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? never : P]: T[P];
+};
+
+type Test = MyOmit<Todo1, 'description'>;
+
+interface Todo1 {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
+  Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>,
+];
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+  completed: boolean;
+}
+
+interface Expected2 {
+  title: string;
+}
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/3/answer
+  > View solutions: https://tsch.js.org/3/solutions
+  > More Challenges: https://tsch.js.org
+*/
