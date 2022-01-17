3578953950d9b4c6b18992a59566e23b7019aa25
BTBMan <1498118157@qq.com>
2022-01-17 21:45:21 +0800

    promiseAll理解当中

--- src/manipulation/map.ts
+++ src/manipulation/map.ts
@@ -21,3 +21,16 @@ type Test2 = {
 type Test3 = {
   [P in keyof T1 as `re-map`]: T1[P];
 };
+
+type Test4 = [boolean, string];
+
+// 如果遍历的是一个元祖类型 则 P 是 number 类型
+type Test5 = {
+  [P in Test4]: P;
+};
+
+type Test6<T> = {
+  [P in keyof T]: P;
+};
+
+type Test7 = Test6<[1, 2, 3]>;
